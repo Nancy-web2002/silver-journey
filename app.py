7becb8971fe02e4d5cec91aa272af838f5f5d628
Nancy-web2002 <nancychilotam@gmail.com>
2025-10-12 13:07:25 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 import random, string
 
 app = Flask(__name__)
+
+CORS (app, resources={r"/*": {"origins": "*"}})
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shipments.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
